0c6516d66a193fd37c54c3167556b0e2ba3b6c81
Nicolas Aimetti <naimetti@onapsis.com>
2019-10-29 09:35:19 -0300

    url typo

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/naimetti/rfc3339_validator',
+    url='https://github.com/naimetti/rfc3339-validator',
     version='0.1.0',
     zip_safe=False,
 )
